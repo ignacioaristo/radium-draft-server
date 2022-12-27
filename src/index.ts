e0dc68abca377c8accb55c26b5f22bdec8d02799
Ignacio Aristo <ignacioaristo@gmail.com>
2022-12-27 11:55:51 -0300

    develop branch created

--- src/index.ts
+++ src/index.ts
@@ -8,5 +8,5 @@ dotenv.config();
 const port = process.env.PORT || 4000;
 
 app.listen(port, () => {
-  console.log(`⚡️ Server is running at http://localhost:${port} ✅`);
+  console.log(`Server is running at http://localhost:${port} ✅`);
 });
