a6f505587ea9bdba3a45e1fede559c54f058a7e7
julianv97 <julian.vicente@radiumrocket.com>
2023-01-06 10:36:17 -0300

    RRD-48: create edit player endpoint

--- src/routes/player.ts
+++ src/routes/player.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 
-import { createPlayer, getPlayer, getPlayers } from 'src/controllers/player';
+import { createPlayer, editPlayer, getPlayer, getPlayers } from 'src/controllers/player';
 import { isFirebaseTokenValid, isPlayerAuthMiddleware } from 'src/middlewares/auth';
 import { validatePlayerSchema } from 'src/middlewares/player';
 
@@ -9,5 +9,6 @@ const router = express.Router();
 router.post('/', isFirebaseTokenValid, validatePlayerSchema, createPlayer);
 router.get('/all', isPlayerAuthMiddleware, getPlayers);
 router.get('/', isPlayerAuthMiddleware, getPlayer);
+router.patch('/', isPlayerAuthMiddleware, validatePlayerSchema, editPlayer);
 
 export default router;
